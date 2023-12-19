9b4e1f0e1c5a9268f0c6ceb680d2fcf4e09d761e
Muhammad Trio Maulana Putra <triomp58@gmail.com>
2023-12-19 09:52:47 +0700

    first commit

---
+++ src/Applications/use_case/_test/AddThreadUseCase.test.js
@@ -0,0 +1,15 @@
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const AddThreadUseCase = require('../AddThreadUseCase');
+
+describe('AddThreadUseCase', () => {
+  it('should orchestrating the add thread action correctly', async () => {
+    /**
+     * @TODO 3
+     * Lengkapi pengujian `AddThreadUseCase` agar dapat memastikan
+     * flow/logika yang dituliskan pada `AddThreadUseCase` benar!
+     *
+     * Tentunya, di sini Anda harus melakukan Test Double
+     * untuk memalsukan implmentasi fungsi `threadRepository`.
+     */
+  });
+});
---
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -0,0 +1,13 @@
+const ThreadRepository = require('../ThreadRepository');
+
+describe('ThreadRepository abstract', () => {
+  it('should throw error when invoke abstract behavior', async () => {
+    /**
+     * @TODO 1
+     * Lengkapi pengujian untuk `ThreadRepository` abstract
+     * Pastikan semua fungsi yang ada di `ThreadRepository`
+     * membangkitkan `Error` THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED
+     *
+     */
+  });
+});
